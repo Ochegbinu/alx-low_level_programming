e3afdeed7b6cfb6ce3136ab88bfa1254b7e6ad73
ochegbinu <ochegbinuisaacgm@git config --global user.email ochegbinuisaacgm@>
2022-09-08 16:14:43 -0700

    test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,25 @@
-i#include <stdio.h>
+#include <stdio.h>
+
+
 
 /**
  *
- *  * main - main block
+ * * main - Entry point
  *
- *   * Return: 0
+ * *
  *
- *    */
+ * * Return: Always 0 (Success)
+ *
+ * */
+
+
 
 int main(void)
 
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	    printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 
-	return (0);
+	        return (0);
 
 }
