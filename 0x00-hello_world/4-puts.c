b21105d97e79848425e59c26771946c970c85c2e
ochegbinu <ochegbinuisaacgm@git config --global user.email ochegbinuisaacgm@>
2022-09-08 16:11:31 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,23 @@
-#include <stdio.h>
+#include "stdio.h"
+
+
 
 /**
  *
- *  * main - main block
+ *  * main - Entry point
  *
- *   * Return: 0
+ *   *
  *
- *    */
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
-int main(void)
 
-{
 
-	puts("\"Programming is like building a multilingual puzzle");
+int main(void){
 
-	return (0);
+	   puts("\"Programming is like building a multilingual puzzle");
+
+	      return (0);
 
 }
