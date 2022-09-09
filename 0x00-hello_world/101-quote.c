fdc74af8d2c4da4081ad8bf4ab3fa4f1af725219
ochegbinu <ochegbinuisaacgm@git config --global user.email ochegbinuisaacgm@>
2022-09-08 18:08:36 -0700

    tal

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
 /**
-* main - prints exactly "and that piece of art is useful"
-* - Gideon Obae, 2021-08-26",
-* followed by a new line, to the standard error.
-* Return: 1 if success.
-*/
+ * main - prints exactly "and that piece of art is useful"
+ * - Gideon Obae, 2021-08-26",
+ * followed by a new line, to the standard error.
+ * Return: 1 if success.
+ */
 
 int main(void)
 
