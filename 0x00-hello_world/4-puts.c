8ba19956f854bbc3ae04bda182b696881ba88969
ochegbinu <ochegbinuisaacgm@git config --global user.email ochegbinuisaacgm@>
2022-09-08 17:56:03 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,12 +5,15 @@
 *
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
-* return: 0
+* Return: 0
 */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
 
 }
