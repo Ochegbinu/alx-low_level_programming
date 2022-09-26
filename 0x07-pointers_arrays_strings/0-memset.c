c3c02c7bc4dbf75b40845a75556dca91f1d7e538
ochegbinu <ochegbinuisaacgm@git config --global user.email ochegbinuisaacgm@>
2022-09-26 16:16:42 -0700

    doc

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,11 @@
 #include "main.h"
 
-
-
-
-
-
-
 /**
- * _memset - fills memory with a constant byte
+ * _memset - fills memory with a constant byte.
  * @s: source string
- * @b: the contant bit for filling
- * @n: length of buffer
- * Return: new string
+ * @b: the contant byte for filling
+ * @n: lenght of buffer
+ * Return: new string.
  */
 
 
