278b844a5d516d2870485f905704d6f9ff8e4a09
ochegbinu <ochegbinuisaacgm@git config --global user.email ochegbinuisaacgm@>
2022-09-26 17:25:33 -0700

    test

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,39 +1,81 @@
-#include "holberton.h"
+#include "main.h"
 
 
 
 /**
- *
- *  *_strchr - locates a character in a string
- *
- *   *
- *
- *    *@s: pointer that contains the address of the string
- *
- *     *@c: character that needs to be located with in the string
- *
- *      *
- *
- *       *Return: address of the first occurrence of c in the string
- *
- *        */
+ * _strspn - Locates a character in a string
+ * @s: This is the main C string to be scanned.
+ * @accept: This is the string containing the list of characters to match in s
+ * Return: return count
+ **/
 
-char *_strchr(char *s, char c)
+
+
+unsigned int _strspn(char *s, char *accept)
+
+
 
 {
 
- while (*s != '\0') 
- {
-	 if (*s == c )
-		 return (s);
- }
-      s++;
-}
-   if (*s == c)
-{
-    return (s);
+		int i, j;
+
+			int count = 0;
+
+				char *str1, *str2;
+
+
+
+					str1 = s;
+
+						str2 = accept;
+
+
+
+							i = 0;
+
+								while (str1[i] != '\0') /*Declaring WHILE *s */
+
+										{
+
+													j = 0;
+
+															while (str2[j] != '\0') /*Declaring WHILE *accept*/
+
+																		{
+
+																						if (str2[j] == str1[i]) /*Evaluate condition*/
+
+																										{
+
+																															count++; /*count number*/
+
+																																			break;
+
+																																						}
+
+
+
+																									j++;    /*add j+1*/
+
+																											}
+
+																	if (s[i] != accept[j]) /*If aren't equals*/
+
+																				{
+
+																								break;
+
+																										}
+
+
+
+																			i++; /*add x+1*/
+
+																				}
+
+
+
+									return (count); /*return the value of count*/
 
 }
-    return (0);/* values null*/
 
-}
