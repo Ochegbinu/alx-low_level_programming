5ee9c34fb80c2eb85d86a164cad9a447728f65c0
ochegbinu <ochegbinuisaacgm@git config --global user.email ochegbinuisaacgm@>
2022-09-29 16:31:02 -0700

    task j

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,82 +1,50 @@
 #include "main.h"
 
+/**
+ * sqrt_check - Checks for the square root of c
+ * @g: Guess at sqrt
+ * @c: Number to find sqrt of
+ * Return: -1 or sqrt of c
+ */
 
 
 
+int sqrt_check(int g, int c)
+
+{
+
+		if (g * g == c)
+
+					return (g);
+
+			if (g * g > c)
+
+						return (-1);
+
+				return (sqrt_check(g + 1, c));
+
+}
 
 
 
 /**
- * _sqrt_recursion - main funct
- * @n: int n
- * Return: int
+ * _sqrt_recursion - Returns the natural square of a number
+ * @n: integer to find sqrt of
+ * Return: Natural square root or -1
  */
 
 
 
 int _sqrt_recursion(int n)
 
-
-
 {
 
+		if (n == 0)
 
-
-			return (_sqrt(n, 1));
-
-
-
-}
+					return (0);
 
 
 
-
-
-
-
-/**
- * _sqrt - _sqrt_recursion
- * @n: integer paramtr
- * @i: integer parameter
- * Return: sqrt
- */
-
-
-
-int _sqrt(int n, int i)
-
-
-
-{
-
-
-
-			if (n < 0)
-
-
-
-									return (-1);
-
-
-
-						if ((i * i) > n)
-
-
-
-													return (-1);
-
-
-
-										if (i * i == n)
-
-
-
-																		return (i);
-
-
-
-															return (_sqrt(n, i + 1));
-
-
+			return (sqrt_check(1, n));
 
 }
