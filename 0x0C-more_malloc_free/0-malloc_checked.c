9d9ac205bdf4abcf210b01af38c9c5b41fca5f27
ochegbinu <ochegbinuisaacgm@git config --global user.email ochegbinuisaacgm@>
2022-10-06 16:04:19 -0700

    corection

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,18 +5,10 @@
 
 
 /**
- *
- * * *malloc_checked - allocates memory using malloc
- *
- * *
- *
- * * @b: number of bytes to allocate
- *
- * *
- *
- * * Return: a pointer to the allocated memory
- *
- * */
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ * Return: a pointer to the allocated memory
+ */
 
 
 
