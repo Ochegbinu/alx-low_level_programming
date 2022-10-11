79420051744a5e3e85acf40411a44260b2436ab9
ochegbinu <ochegbinuisaacgm@git config --global user.email ochegbinuisaacgm@>
2022-10-10 18:11:25 -0700

    task

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,28 +3,30 @@
 
 
 /**
- * init_dog - initializes a variable of type struct dog.
- * @d: struct dog.
- * @name: name of the dog.
- * @age: age of the dog.
- * @owner: owner of the dog.
- * Return: no return.
+ * init_dog - inits a variable of
+ * type dog
+ * @d: dog identification
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner's name
  */
+
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 
 {
 
-		if (d)
+	if (d != NULL)
 
-				{
+	{
 
-							d->name = name;
+		(*d).name = name;
 
-									d->age = age;
+		(*d).age = age;
 
-											d->owner = owner;
+		(*d).owner = owner;
 
-												}
+	}
 
 }
